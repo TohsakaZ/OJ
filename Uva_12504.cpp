044e2f6f90b9bafe71d375af996f70b29ad97db0
ZhengHJ <524836851@qq.com>
2020-09-04 17:35:06 +0800

    add Uva 12504

---
+++ Uva_12504.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+
+using namespace std;
+
+map<int,int> decode_set(string& set,map<string,int>& talbe_index,vector<string>& table)
+{
+    map<int,int> result;
+    set=set.substr(1);
+    set[set.length()]=',';
+    int idx=0;
+    while (set.find(',',idx)!=string::npos)
+    {
+        int cola_idx=set.find(':');
+        int comma_idx=set.find(',');
+        string str1 = set.substr(idx,cola_idx-idx);
+        string str2 = set.substr(cola_idx+1,comma_idx-cola_idx-1);
+        if (talbe_index.count(str1)==0){
+            talbe_index[str1]=table.size();
+            table.push_back(str1);
+        }
+        if (talbe_index.count(str2)==0){
+            talbe_index[str2]=table.size();
+            table.push_back(str2);
+        }
+        result[talbe_index[str1]]=talbe_index[str2];
+    }
+
+    return result ;
+}
+
+void cout_set(set<int>& sets,vector<string>& table,char mark)
+{
+    for (auto item:sets){
+        if (item!=*sets.begin()){
+            cout << mark;
+        }
+        else{
+            cout << ',';
+        }
+        cout << table[item];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ifstream fin("input.txt");
+    cin.rdbuf(fin.rdbuf());
+
+    int test_nubmer=0;
+    while (test_nubmer--)
+    {
+        string str_set1,str_set2;
+        getline(cin,str_set1);
+        getline(cin,str_set2);        
+
+        vector<string> table;
+        map<string,int> table_index;
+        auto set1 = decode_set(str_set1,table_index,table);
+        auto set2 = decode_set(str_set2,table_index,table);
+
+        set<int> add;
+        set<int> remove;
+        set<int> v_change;
+        for (const auto& item: set2)
+        {
+            if (set1.count(item.first)==0){
+                add.insert(item.first);    
+            }
+            else{
+                if (item.second!=set1[item.first]){
+                    v_change.insert(item.first);
+                }
+            } 
+        }
+
+        for (const auto& item : set1)
+        {
+            if (set2.count(item.first)==0){
+                remove.insert(item.first);
+            }
+        }
+
+        if (add.size()==0 && remove.size()==0 && v_change.size()==0){
+            cout << "No changes"<< endl;
+        } 
+        else{
+            cout_set(add,table,'+'); 
+            cout_set(remove,table,'-'); 
+            cout_set(v_change,table,'*'); 
+        }
+
+        cout << endl;
+    }
+   }
