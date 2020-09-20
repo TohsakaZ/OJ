545857436b2b9547d2fef61d1c52e6cb2ea95f6e
ZhengHJ <524836851@qq.com>
2020-09-20 11:10:57 +0800

    Uva 1598 AC

--- Uva_1598.cpp
+++ Uva_1598.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 
+
 class buy_trade
 {
 public:
@@ -19,10 +20,10 @@ public:
     }
     int price=0;
     int size=0;
-    int number =0;
+    int number=0;
 
-    bool operator> (const buy_trade& Other){
-        return (price > Other.price || (price == Other.price && number < Other.number));
+    bool operator < (const buy_trade& Other) const{
+        return (price < Other.price || (price == Other.price && number > Other.number));
     }
 };
 
@@ -37,10 +38,10 @@ public:
     }
     int price=0;
     int size=0;
-    int number =0;
+    int number=0;
 
-    bool operator> (const sell_trade& Other){
-        return (price < Other.price || (price == Other.price && number < Other.number));
+    bool operator < (const sell_trade& Other) const{
+        return (price > Other.price || (price == Other.price && number > Other.number));
     }
 };
 
@@ -49,21 +50,30 @@ public:
 
 int main()
 {
-    ifstream fin("input.txt");
-    cin.rdbuf(fin.rdbuf());
-    int cnt=0;
+    //ifstream fin("input.txt");
+    //cin.rdbuf(fin.rdbuf());
+    //ofstream fout("output.txt");
+    //cout.rdbuf(fout.rdbuf());
+    int cnt = 0;
     int number_message=0;
     while (cin >> number_message){
         if (cnt!=0){
             cout << endl;
         }
+        cnt++;
+        string temp;
+        getline(cin, temp);
+        vector<int> size_list;
+        vector<pair<int, string> > type_list;
         set<int> unvalid_map;
         priority_queue<buy_trade> buy_list;
         priority_queue<sell_trade> sell_list;
-
-        while (number_message--){
+        map<int, int> buy_table;
+        map<int, int> sell_table;
+        for (int i = 0; i < number_message; i++)
+        {
             string templine;
-            getline(cin,templine);
+            getline(cin, templine);
             stringstream line_split(templine);
             string key_word;
             line_split >> key_word;
@@ -74,34 +84,104 @@ int main()
                     while (!sell_list.empty() and unvalid_map.count(sell_list.top().number)!=0 ){
                         sell_list.pop();
                     }
-
                     if (sell_list.empty() || price < sell_list.top().price){
                         break;
                     }
-                    
-                    int common_size = min(size,sell_list.top().size);
+                       
+                    auto top_trade = sell_list.top();
+                    sell_list.pop();
+                    int common_size = min(size, top_trade.size);
+                    cout << "TRADE " << common_size << " " << top_trade.price << endl;
                     size -= common_size;
-
+                    top_trade.size -= common_size;
+                    sell_table[top_trade.price] -= common_size;
+                    size_list[top_trade.number-1]-=common_size;
+                    if (top_trade.size > 0)
+                    {
+                        sell_list.push(top_trade);
+                    }
                 }
+                if (size > 0){
+                    buy_list.push(buy_trade(size, price, i + 1));
+                    buy_table[price] += size;
+                }
+                size_list.push_back(size);
+                type_list.push_back(make_pair(price, "BUY"));
             }
             else if (key_word == "SELL"){
-                int size,price;
+                int size, price;
                 line_split >> size >> price;
-                trade_info* new_trade = new trade_info(size,true);
-                message_list.push_back(new_trade);
-                sell_table.push(price);
-                sell_list[price].push(new_trade);
+                while (size > 0 ){
+                    while (!buy_list.empty() and unvalid_map.count(buy_list.top().number)!=0){
+                        buy_list.pop();
+                    }
+                    if (buy_list.empty() || price > buy_list.top().price){
+                        break;
+                    }
+
+                    auto top_trade = buy_list.top();
+                    buy_list.pop();
+                    int common_size = min(size, top_trade.size);
+                    cout << "TRADE " << common_size << " " << top_trade.price << endl;
+                    size -= common_size;
+                    top_trade.size -= common_size;
+                    buy_table[top_trade.price] -= common_size;
+                    size_list[top_trade.number - 1] -= common_size;
+                    if (top_trade.size > 0)
+                    {
+                        buy_list.push(top_trade);
+                    }
+                }
+                if (size >0){
+                    sell_list.push(sell_trade(size, price, i + 1));
+                    sell_table[price] += size;
+                }
+                size_list.push_back(size);
+                type_list.push_back(make_pair(price, "SELL"));
             }
             else if (key_word == "CANCEL"){
                 int number;
                 line_split >> number;
-                unvalid_map.isnert(number);
+                unvalid_map.insert(number);
+                size_list.push_back(-1);
+                type_list.push_back(make_pair(-1, "NONE"));
+                if (size_list[number-1] > 0){
+                    if (type_list[number-1].second == "BUY"){
+                        buy_table[type_list[number - 1].first] -= size_list[number - 1];
+                    }
+                    else if(type_list[number-1].second == "SELL"){
+                        sell_table[type_list[number - 1].first] -= size_list[number - 1];
+                    }
+                    size_list[number - 1] = 0;
+                }
             }
 
-            // cout quote
+            // count sum;
+            cout << "QUOTE ";
+            int bie_size = 0, bie_price = 0;
+            while (!buy_list.empty() && unvalid_map.count(buy_list.top().number)!=0){
+                buy_list.pop();
+            }
+            if (!buy_list.empty()){
+                bie_price = buy_list.top().price;
+                bie_size = buy_table[bie_price];
+            }
+            cout << bie_size << " " << bie_price;
+            cout << " - ";
+
+            int ask_size = 0, ask_price = 99999;
+            while (!sell_list.empty() && unvalid_map.count(sell_list.top().number) != 0)
+            {
+                sell_list.pop();
+            }
+            if (!sell_list.empty()){
+                ask_price = sell_list.top().price;
+                ask_size = sell_table[ask_price];
+            }
+            cout << ask_size << " " << ask_price << endl;
         }
         string empty_line;
-        getline(cin,empty_line);
+        //getline(cin,empty_line);
     }
 
 }
