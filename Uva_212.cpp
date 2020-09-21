882790ae886d7275ea09934669a6218039b474e4
ZhengHJ <524836851@qq.com>
2020-09-21 15:22:51 +0800

    Uva 212 AC

--- Uva_212.cpp
+++ Uva_212.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 #include <climits>
 #include <fstream>
 #include <sstream>
@@ -7,9 +8,7 @@
 #include <iomanip>
 #include <map>
 #include <set>
-
 using namespace std;
-
 class clock_time
 {
 public:
@@ -19,27 +18,22 @@ public:
     clock_time(int hour):
         minutes(hour*60)
     {}
-
     clock_time operator+(int Minutes) const
     {
         clock_time new_time;
         new_time.minutes = this->minutes + Minutes;
         return new_time;
     }
-
     bool operator<(const clock_time & Other) const
     {
         return this->minutes < Other.minutes;
     }
-
     bool operator<=(const clock_time& Other) const{
         return this->minutes <= Other.minutes;
     }
-
     bool operator==(const clock_time& Other) const{
         return this->minutes == Other.minutes;
     }
-
     string str_format(){
         int hour = minutes / 60;
         int min = minutes - hour * 60;
@@ -47,27 +41,15 @@ public:
         k << hour << ":" << setw(2) << setfill('0') << min;
         return k.str();
     }
-
     int minutes;
 };
 
-class operating_person
+struct cmp 
 {
-public:
-    operating_person(string name,int operate_id,clock_time next_t):
-        name(name),
-        next_t(next_t),
-        operate_id(operate_id)
-    {}
-
-    bool operator<(const operating_person& Other) const{
-        return next_t < Other.next_t || (next_t == Other.next_t and operate_id <Other.operate_id);
+    bool operator()(const clock_time& a,const clock_time& b){
+    return a.minutes > b.minutes;
     }
-    string name;
-    clock_time next_t;
-    int operate_id;
 };
-
 
 class person_record
 {
@@ -79,125 +61,119 @@ public:
     clock_time recover_beg;
     clock_time recover_end;
 };
-
-
 int main()
 {
     ifstream fin("input.txt");
     cin.rdbuf(fin.rdbuf());
-    string templine;
-    while (getline(cin,templine)){
-        int num_operate,num_recover,beg_time,transport_time,preprare_operate,prepare_recover,num_patient;
-        stringstream line_split(templine);
-        line_split >> num_operate >> num_recover >> beg_time >> transport_time >> preprare_operate >> prepare_recover >> num_patient;
-        map<string,pair<int,int> > person_info;
+    int num_operate, num_recover, beg_time, transport_time, preprare_operate, prepare_recover, num_patient;
+    int cnt=0;
+    while (cin >> num_operate >> num_recover >> beg_time >> transport_time >> preprare_operate >> prepare_recover >> num_patient)
+    {
+        vector< pair<int, int> > person_info;
         vector<string> person_list;
-        queue<string> person_prepare_list; 
-        while (num_patient--){
+        queue<int> person_prepare_list;
+        for (int i = 0; i < num_patient;i++){
             string person_name;
             cin >> person_name;
-            person_prepare_list.push(person_name);
+            person_prepare_list.push(i);
             person_list.push_back(person_name);
             int operate_time,recover_time;
             cin >> operate_time >> recover_time;
-            person_info[person_name]=make_pair(operate_time,recover_time);
+            person_info.push_back(make_pair(operate_time,recover_time));
         }
         clock_time current_time(beg_time);
-        vector<operating_person> operate_list; // time for person(operate+transport)
-        map<int,clock_time> recover_time_info;
-        map<int,clock_time> operate_time_info; // time for bed(oeprate+ prepare)
-        map<string,person_record> record;
+        vector<clock_time> operate_time_info(num_operate);
+        vector<int> operate_name(num_operate,-1);
+        vector<bool> operate_working(num_operate, false);
+        vector<clock_time> recover_time_info(num_recover);
+        vector<int> recover_name(num_recover,-1);
+        vector<bool> recover_working(num_recover, false);
 
-        while (!person_prepare_list.empty() || !operate_list.empty())
+        vector<person_record> record(num_patient);
+        priority_queue<clock_time, vector<clock_time>, cmp> time_pool;
+        time_pool.push(current_time);
+        clock_time pre_time;
+        pre_time.minutes = -1;
+        while (!time_pool.empty())
         {
-            //cout << "current time is " << current_time.str_format() << endl;
-            // get the next time;
-            clock_time jump_time;
-            jump_time.minutes=INT_MAX;
+            current_time = time_pool.top();
+            time_pool.pop();
+            if (current_time == pre_time){
+                continue;
+            }
+            pre_time = current_time;
+
             // make sure all avilabe recover bed
-            set<int> avalible_recover_bed;
-            for (int i=1;i<= num_recover;i++){
-                if (recover_time_info[i] < current_time){
-                    avalible_recover_bed.insert(i);
+            queue<int> wait_for_recover;
+            // get the person finish operating and all avliable operate room
+            for (int i = 0; i < num_operate;i++){
+                if (operate_time_info[i] <= current_time && operate_working[i]){
+                    operate_working[i] = false;
+                    if (operate_name[i] !=-1)
+                    {
+                        operate_working[i] = true;
+                        operate_time_info[i] = current_time + preprare_operate;
+                        wait_for_recover.push(operate_name[i]);
+                        operate_name[i] = -1;
+                        time_pool.push(operate_time_info[i]);
+                    }
+               }
+                if (!operate_working[i] && !person_prepare_list.empty()){
+                    operate_working[i] = true;
+                    int person_id = person_prepare_list.front();
+                    person_prepare_list.pop();
+                    record[person_id].oerpate_id= i + 1;
+                    record[person_id].operate_beg = current_time;
+                    record[person_id].operate_end = current_time + person_info[person_id].first;
+                    operate_time_info[i] = record[person_id].operate_end;
+                    operate_name[i] = person_id;
+                    time_pool.push(record[person_id].operate_end);
                 }
             }
-
-            set<operating_person> wait;
-            // get the person finish operating
-            for (auto iter =operate_list.begin() ;iter != operate_list.end();){
-                if (iter->next_t == current_time){
-                    wait.insert(*iter);
-                    iter = operate_list.erase(iter);
-                    continue;
-                }
-                else{
-                    jump_time = min(jump_time,iter->next_t);
-                    iter++;
-                    continue;
-                }
-            }
-
-            // move to the recover
-            auto recover_bed_iter = avalible_recover_bed.begin();
-            for (auto person_item:wait){
-                int recover_bed = *recover_bed_iter;
-                recover_bed_iter++;
-                record[person_item.name].recover_id = recover_bed;
-                record[person_item.name].recover_beg = current_time;
-                record[person_item.name].recover_end = current_time+person_info[person_item.name].second;
-                recover_time_info[recover_bed] = record[person_item.name].recover_end+prepare_recover;
-            }
-
-            // get the avalible operate bed;
-            set<int> avalible_operate_bed;
-            for (int i = 1;i<=num_operate ;i++){
-                if (operate_time_info[i] <= current_time){
-                    avalible_operate_bed.insert(i);
-                }
-                else{
-                    jump_time = min(jump_time,operate_time_info[i]);
+            // get all availabe recover bed and move to recover
+            for (int i = 0; i < num_recover; i++)
+            {
+                if (recover_time_info[i] <= current_time && recover_working[i]){
+                    recover_working[i] = false;
+                    if (recover_name[i] !=-1){
+                        recover_working[i] = true;
+                        recover_time_info[i] = current_time + prepare_recover;
+                        recover_name[i] = -1;
+                        time_pool.push(recover_time_info[i]);
+                    }
+               }
+                if (!recover_working[i] && !wait_for_recover.empty()){
+                    recover_working[i] = true;
+                    // move wait to the bed room;
+                    int person_id= wait_for_recover.front();
+                    wait_for_recover.pop();
+                    record[person_id].recover_id = i + 1;
+                    record[person_id].recover_beg = current_time + transport_time;
+                    record[person_id].recover_end = current_time + transport_time + person_info[person_id].second;
+                    recover_time_info[i] = record[person_id].recover_end;
+                    recover_name[i] = person_id;
+                    time_pool.push(record[person_id].recover_end);
                 }
             }
-            // move to operate
-            auto operate_iter= avalible_operate_bed.begin();
-            while (operate_iter!=avalible_operate_bed.end() && !person_prepare_list.empty()){
-                auto person_name = person_prepare_list.front();
-                person_prepare_list.pop();
-                record[person_name].oerpate_id = *operate_iter;
-                record[person_name].operate_beg = current_time;
-                record[person_name].operate_end = current_time + person_info[person_name].first;
-                operate_time_info[*operate_iter] = record[person_name].operate_end+preprare_operate;
-                operate_list.push_back(operating_person(person_name,*operate_iter,record[person_name].operate_end+transport_time));
-
-                jump_time=min(jump_time,operate_time_info[*operate_iter]);
-                jump_time=min(jump_time,operate_list[operate_list.size()-1].next_t);
-
-                operate_iter++;
-            }
-
-            current_time = jump_time;
         }
         cout << " Patient          Operating Room          Recovery Room" <<endl;
         cout << " #  Name     Room#  Begin   End      Bed#  Begin    End" << endl;
         cout << " ------------------------------------------------------" << endl;
-        clock_time beg(beg_time);
+        clock_time beg;
+        beg.minutes = INT_MAX;
         clock_time end;
         map<int,int> room_table;
         map<int,int> bed_table;
         for (int i=0;i < person_list.size() ;i++){
             string person_name = person_list[i];
-            end = max(record[person_name].recover_end,end);
-            room_table[record[person_name].oerpate_id]+= record[person_name].operate_end.minutes - record[person_name].operate_beg.minutes;
-            bed_table[record[person_name].recover_id]+= record[person_name].recover_end.minutes - record[person_name].recover_beg.minutes;
-            cout << setiosflags(ios::right) <<setw(2) << i+1 << setw(2) << "";
-            cout << setw(9) << setiosflags(ios::left) << person_name 
-                 << setw(3) << setiosflags(ios::left) << record[person_name].oerpate_id  <<setw(2) << ""
-                 << setw(1) << "" << setw(5) << record[person_name].operate_beg.str_format() << setw(3) << ""
-                 << setw(5) << record[person_name].operate_end.str_format() << setw(5) << ""
-                 << setw(2) << setiosflags(ios::right) << record[person_name].recover_id << setw(3) << ""
-                 << setw(5) << record[person_name].recover_beg.str_format() << setw(3) << "" 
-                 << setw(5) << record[person_name].recover_end.str_format() << endl;
-             
+            person_record& record_person = record[i];
+            beg = min(record_person.operate_beg, beg);
+            end = max(record_person.recover_end, end);
+            room_table[record_person.oerpate_id]+= record_person.operate_end.minutes - record_person.operate_beg.minutes;
+            bed_table[record_person.recover_id]+= record_person.recover_end.minutes - record_person.recover_beg.minutes;
+            printf("%2d  %-9s%3d   %5s   %5s     %2d   %5s   %5s\n",i+1,person_name.c_str(),record_person.oerpate_id,record_person.operate_beg.str_format().c_str(),
+                record_person.operate_end.str_format().c_str(),record_person.recover_id,record_person.recover_beg.str_format().c_str(),
+                record_person.recover_end.str_format().c_str());
         }
         double total_minutes = (end.minutes-beg.minutes)/100.0;
 
@@ -206,24 +182,21 @@ int main()
         cout << "Type  # Minutes  % Used" << endl;
         cout << "-------------------------" << endl;
         for (int i =0;i<num_operate;i++){
-            cout << setw(4) << "Room"  << setw(1) << ""
-                 << setw(2) << setiosflags(ios::right) << i+1
-                 << setw(8) << room_table[i+1]
-                 << setw(3) << "" 
-                 << setw(5) << setiosflags(ios::fixed) << setprecision(2) << room_table[i+1]/total_minutes 
-                 << endl;
-        }
+            if (num_patient==0){
+                printf("%-5s%2d%8d   %5.2f\n","Room",i+1,room_table[i+1],0.0);
+            }
+            else{
+                printf("%-5s%2d%8d   %5.2f\n","Room",i+1,room_table[i+1],room_table[i+1]/total_minutes);
+            }
+       }
         for (int i =0;i<num_recover;i++){
-            cout << setw(3) << "Bed"  << setw(2) << ""
-                 << setw(2) << setiosflags(ios::right) << i+1
-                 << setw(8) << bed_table[i+1]
-                 << setw(3) << "" 
-                 << setw(5) << setiosflags(ios::fixed) << setprecision(2) << bed_table[i+1]/total_minutes 
-                 << endl;
+            if (num_patient==0){
+                printf("%-5s%2d%8d   % 5.2lf\n","Bed",i+1,bed_table[i+1],0.0);
+            }
+            else{
+                printf("%-5s%2d%8d   %5.2lf\n","Bed",i+1,bed_table[i+1],bed_table[i+1]/total_minutes);
+            }
         }
-
+        cout << endl;
     }
-
-
-
 }
